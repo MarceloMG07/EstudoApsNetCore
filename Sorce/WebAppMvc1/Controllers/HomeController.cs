0ef0a1ef060c196338a0969fecf0d0bb8248cb00
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-08-18 14:22:15 -0300

    Rotas / Sobrecargas de Rotas / Passar Atributo Via Rota (Restrição de Rota)

--- Sorce/WebAppMvc1/Controllers/HomeController.cs
+++ Sorce/WebAppMvc1/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using WebAppMvc1.Models;
 
 namespace WebAppMvc1.Controllers
 {
+    [Route("")]
+    [Route("MinhaRotaPorAtributo")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -13,17 +15,30 @@ namespace WebAppMvc1.Controllers
             _logger = logger;
         }
 
+        [Route("")]
+        [Route("Inicio")]
         public IActionResult Index()
         {
             return View();
         }
 
+        [Route("TesteInicio")]
+        [Route("TesteInicio/{id:int}/{categoria?}")]
+        //https://localhost:7207/TesteInicio/5/Monitores?fiscal=true
+        //https://localhost:7207/TesteInicio/5?fiscal=true
+        public IActionResult Teste(int id, string categoria, bool fiscal)
+        {
+            return View("Index");
+        }
+
+        [Route("Privacidade")]
         public IActionResult Privacy()
         {
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("Erro")]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
