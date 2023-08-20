857b9942b72bbca97384df530fb057d8ea1f0038
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-08-20 19:58:08 -0300

    Usando Scaffold para Criar Controller e View de Filmes

---
+++ Sorce/WebAppMvcScaffold/Data/WebAppMvcScaffoldContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppMvcScaffold.Models;
+
+namespace WebAppMvcScaffold.Data
+{
+    public class WebAppMvcScaffoldContext : DbContext
+    {
+        public WebAppMvcScaffoldContext (DbContextOptions<WebAppMvcScaffoldContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppMvcScaffold.Models.Filme> Filme { get; set; } = default!;
+    }
+}
