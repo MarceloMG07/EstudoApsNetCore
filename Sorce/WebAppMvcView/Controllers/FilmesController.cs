62625aa1fa5c5ebb2f508fdfd1cf99510ae02691
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-08-20 19:06:07 -0300

    Criando um Formulário

---
+++ Sorce/WebAppMvcView/Controllers/FilmesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMvcView.Controllers
+{
+    //[Route("")]
+    //[Route("Filmes")]
+    public class FilmesController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
