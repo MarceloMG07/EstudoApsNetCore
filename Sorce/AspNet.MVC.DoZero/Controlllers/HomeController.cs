a4eb98d37fbdc244608a30133d11bd9a7bcc8948
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-08-21 20:55:46 -0300

    Projeto Limpo

---
+++ Sorce/AspNet.MVC.DoZero/Controlllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNet.MVC.DoZero.Controlllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
