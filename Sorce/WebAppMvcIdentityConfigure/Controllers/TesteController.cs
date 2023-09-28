cbd92e753950736d52613d4123dbc90858db41bb
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-09-27 21:54:54 -0300

    Usando ILogger

---
+++ Sorce/WebAppMvcIdentityConfigure/Controllers/TesteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppMvcIdentityConfigure.Controllers
+{
+    public class TesteController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public TesteController(ILogger<TesteController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogError("Este erro aconteceu!");
+
+            return View();
+        }
+    }
+}
