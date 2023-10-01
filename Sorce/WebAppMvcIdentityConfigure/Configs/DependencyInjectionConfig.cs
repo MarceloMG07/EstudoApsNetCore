618a67e4c35928baf3a8070c4fae7bfa69511635
IATec - Marcelo Moura Goncalves <marcelo.goncalves@iatec.com>
2023-10-01 06:36:40 -0300

    Correção DependencyInjection

--- Sorce/WebAppMvcIdentityConfigure/Configs/DependencyInjectionConfig.cs
+++ Sorce/WebAppMvcIdentityConfigure/Configs/DependencyInjectionConfig.cs
@@ -1,7 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using WebAppMvcIdentityConfigure.Areas.Identity.Data;
 using WebAppMvcIdentityConfigure.Helpers;
 
 namespace WebAppMvcIdentityConfigure.Configs
@@ -12,26 +9,6 @@ namespace WebAppMvcIdentityConfigure.Configs
         {
             services.AddSingleton<IAuthorizationHandler, PermissaoNecessariaHelperHandler>();
             
-            return services;
-        }
-        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            // Adicionando suporte ao contexto do Identity via EF
-            var connectionString = configuration.GetConnectionString("WebAppMvcIdentityContextConnection") ?? throw new InvalidOperationException("Connection string 'WebAppMvcIdentityContextConnection' not found.");
-            services.AddDbContext<WebAppMvcIdentityContext>(options => options.UseMySQL(connectionString));
-
-            // Adicionando configuração padrão do Identity
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddDefaultUI()
-                .AddEntityFrameworkStores<WebAppMvcIdentityContext>();
-
             return services;
         }
     }
--- Sorce/WebAppMvcIdentityConfigure/Configs/IdentityConfig.cs
+++ Sorce/WebAppMvcIdentityConfigure/Configs/IdentityConfig.cs
@@ -1,4 +1,7 @@
-﻿using WebAppMvcIdentityConfigure.Helpers;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebAppMvcIdentityConfigure.Areas.Identity.Data;
+using WebAppMvcIdentityConfigure.Helpers;
 
 namespace WebAppMvcIdentityConfigure.Configs
 {
@@ -14,6 +17,26 @@ namespace WebAppMvcIdentityConfigure.Configs
                 options.AddPolicy("PodeEscrever", policy => policy.Requirements.Add(new PermissaoNecessariaHelper("PodeEscrever")));
                 //options.AddPolicy("PodeEscrever", policy => policy.Requirements.Add(new PermissaoNecessaria("PodeEscrever")));
             });
+
+            return services;
+        }
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            // Adicionando suporte ao contexto do Identity via EF
+            var connectionString = configuration.GetConnectionString("WebAppMvcIdentityContextConnection") ?? throw new InvalidOperationException("Connection string 'WebAppMvcIdentityContextConnection' not found.");
+            services.AddDbContext<WebAppMvcIdentityContext>(options => options.UseMySQL(connectionString));
+
+            // Adicionando configuração padrão do Identity
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddDefaultUI()
+                .AddEntityFrameworkStores<WebAppMvcIdentityContext>();
 
             return services;
         }
